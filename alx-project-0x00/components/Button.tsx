fae0ed22f729d082ad594a463cb5e2cdc97e74d3
Tracy Wanjiru Karanja <chirutracy@gmail.com>
2025-07-16 17:12:08 +0300

    :art: Update Button component styles and improve code formatting

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import { ButtonProps } from "@/interfaces";
 
-// This component is a simple button that displays a title and applies styles.
 const Button: React.FC<ButtonProps> = ({ title, style }) => {
-    return (
-        <button className={`py-2 px-4 rounded ${style}`}>
-            {title}
-        </button>
-    )
-}
+  return (
+    <button className={`px-4 py-2 text-white font-semibold ${style}`}>
+      {title}
+    </button>
+  );
+};
+
 export default Button;
